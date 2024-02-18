cb23162d4c4f9d638a483e1454ead8b599595722
NiihaarrX <141237338+NiihaarrX@users.noreply.github.com>
2024-02-18 11:20:39 +0530

    Create Sieve_01.cpp

---
+++ Number Theory/PrimeTheory/Sieve_01.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Time Complexity = O(n log(log n))
+// Space Complexity = O(n)
+
+vector<int> Sieve(int n) {
+    vector<bool> prime(n+1,true);
+    prime[0] = false;prime[1] = false;
+    for(int i = 2; i <= n;i++) {
+        if(prime[i] && (long long)i * i < n) {
+            for(int j = i * i;j <= n; j += i) prime[j] = false;
+        }
+    }
+    vector<int> ans;
+    for(int i = 0; i < prime.size();i++) {
+        if(prime[i] == true) ans.push_back(i);
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> primes = Sieve(n);
+    for(int i = 0; i < primes.size();i++) {
+        cout << primes[i] << " ";
+    }cout << endl;
+    return 0;
+}
