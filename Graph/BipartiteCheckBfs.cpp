71dc4f5065e2602734c759c8a2fe1afce6915643
NiihaarrX <141237338+NiihaarrX@users.noreply.github.com>
2024-02-19 00:16:05 +0530

    Create BipartiteCheckBfs.cpp

---
+++ Graph/BipartiteCheckBfs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool Bipartite(vector<vector<int>> &G,int start) {
+    vector<int> colour(G.size(),-1);
+    colour[start] = 1;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()) {
+        auto front = q.front();
+        q.pop();
+        for(int i = 0; i < G[front].size();i++) {
+            if(colour[G[front][i]] == -1) {
+                colour[G[front][i]] = 1 - colour[front];
+                q.push(G[front][i]);
+            }
+            else if(colour[G[front][i]] == colour[front]) return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int V,E,start;
+    cin >> V >> E >> start;
+    vector<vector<int>> G(V);
+    for(int i = 0; i < E;i++) {
+        int a,b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    bool ans = Bipartite(G,start);
+    if(ans) cout << "Bipartite Graph" << endl;
+    else cout << "Not Bipartite Graph" << endl;
+    return 0;
+}
+// 1 2 3
+// 4 5
