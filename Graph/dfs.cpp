8c4ea2d249a62c620a7d465782c677dfce778191
NiihaarrX <141237338+NiihaarrX@users.noreply.github.com>
2024-06-11 07:23:53 +0530

    Update dfs.cpp

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,27 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-void DFS(vector<vector<int>> &G,vector<bool> &vis,int start) {
-    vis[start] = true;
-    cout << start << " ";
-    for(int i = 0;i < G[start].size();i++) {
-        if(!vis[G[start][i]]) {
-            DFS(G,vis,G[start][i]);
-        }
-    }
+void dfs(vector<vector<int>> &g,int src,vector<bool> &vis) {
+  cout << src << " ";
+  vis[src] = true;
+  for(int &i : g[src]) {
+    if(!vis[i]) dfs(g,i,vis);
+  }
 }
 
 int main() {
-    int V,E,start;
-    cin >> V >> E >> start;
-    vector<vector<int>> G(V);
-    for(int i = 0; i < E;i++) {
-        int a,b;
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
-    }
-    vector<bool> vis(V,false);
-    DFS(G,vis,start); 
+  int vertices = 9;
+  // int edges = 10;
+  vector<vector<int>> g(vertices);
+  g[0] = {1, 2};
+  g[1] = {0,3,6};
+  g[2] = {0,4};
+  g[3] = {1,7};
+  g[4] = {2,5,7};
+  g[5] = {4,8};
+  g[6] = {1,8};
+  g[7] = {3,4};
+  g[8] = {5,6};
+  int start = 0;
+  vector<bool> vis(g.size(),false);
+  dfs(g,start,vis);
+  
 }
